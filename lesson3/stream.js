a6d59778892a627f209b7280f601225e3d25d1a5
shadrack <shadrackkinsimba@gmail.com>
2025-01-09 17:46:18 +0300

    working with  NPM

--- lesson3/stream.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-
-const rs =fs.createReadStream('./files/lorem.txt',{encoding: 'utf8'})
-
-const ws = fs.createWriteStream('./files/lorem-new.txt')
-
-// rs.on('data',(dataChuck) => ws.write(dataChuck))
-rs.pipe(ws)
