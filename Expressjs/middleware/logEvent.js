f28e8dbf5482160ec4203e720b361f1ca9b994e7
shadrack <shadrackkinsimba@gmail.com>
2025-01-19 14:20:20 +0300

    bulding middleware in expressjs

---
+++ Expressjs/middleware/logEvent.js
@@ -0,0 +1,33 @@
+const { format }  = require('date-fns')
+const { v4:uuid }  = require('uuid')
+
+const fs = require('fs')
+const fsPromise = require('fs').promises
+const path = require('path')
+
+
+const logEvent = async (message,logName)=>{
+    const dateTime  = `${format(new Date(),'yyyy-MM-dd\tHH:mm:ss')}`
+    const logMessage = `${dateTime}\t${uuid()}\t${message}\n`
+    console.log(logMessage)
+    try{
+        if (!fs.existsSync(path.join(__dirname,'..','logs'))){
+            await fsPromise.mkdir(path.join(__dirname,'..','logs'))
+        }
+        //tesing  ttt 
+        await fsPromise.appendFile(path.join(__dirname,'..','logs',logName),logMessage)
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+const logger = (req, res, next) => {
+    logEvent(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+
+
+module.exports = {logger,logEvent}
---
+++ Expressjs/server.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const { console } = require('inspector');
+const path = require('path');
+const cors = require('cors');
+const app = express()
+const PORT = process.env.PORT || 3500;
+
+const  { logger } = require('./middleware/logEvent');
+const   errorHandler  = require('./middleware/errorHandler');
+
+
+// custom middleware logger
+// app.use(logger);
+app.use(logger)
+
+// Cross Origin Resource Sharing
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin ) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:  
+// ‘content-type: application/x-www-form-urlencoded’
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json 
+app.use(express.json());
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    //res.sendFile('./views/index.html', { root: __dirname });
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html'); //302 by default
+});
+
+// Route handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello World!');
+});
+
+
+// chaining route handlers
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('three');
+    res.send('Finished!');
+}
+
+app.get('/chain(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+
+app.route('/book')
+  .get((req, res) => {
+    res.send('Get a random book')
+  })
+  .post((req, res) => {
+    res.send('Add a book')
+  })
+  .put((req, res) => {
+    res.send('Update the book')
+  })
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
